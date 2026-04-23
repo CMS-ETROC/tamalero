847a739e53e71b74370599e556fec49017c4a890
Jongho Lee <jongapples@gmail.com>
2026-04-23 09:07:19 -0500

    call newly added functions

--- run_etroc_ce_beam_daq.py
+++ run_etroc_ce_beam_daq.py
@@ -123,6 +123,8 @@ def main():
     # 2. Initialize Hardware
     system = ETROCSystem(config)
     system.connect()
+    system.check_PS_status()
+    system.pll_fc_calibration_and_set_to_high_power_mode()
 
     # 3. Calibration / Configuration
     cal_mgr = CalibrationManager(system, config)
